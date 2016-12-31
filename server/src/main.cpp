0cb6b474796cdfd0d661b199d3591dc625c7f29f
Jessica VAN-DEN-ZANDE <jessica.van-den-zande@epitech.eu>
2016-12-31 11:22:35 +0100

    update binary argument

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,28 +5,22 @@
 // Login   <van-de_j@epitech.net>
 // 
 // Started on  Mon Dec 19 18:27:07 2016 Jessica VAN-DEN-ZANDE
-// Last update Tue Dec 20 13:16:40 2016 Jessica VAN-DEN-ZANDE
+// Last update Sat Dec 31 11:19:40 2016 Jessica VAN-DEN-ZANDE
 //
 
-#include <iostream>
-#include <string>
 #include "LinuxConnection.hpp"
 
 using namespace std;
 
-int main(int ac, char **av) 
+int		main() 
 {
   Network	network;
+  Configuration	config("config.ini", false);
   bool		state;
 
-  if (ac < 3)
-    {
-      cerr << "./a.out [port] [queue]\n" << endl;
-      return -1;
-    }
-  state = network.initServer(atoi(av[1]), atoi(av[2]));
-  network.runServer(state);
-  cout << "Server is running." << endl;
+  int port = std::stoi(config.getConfigKey("port"), nullptr, 0);
+  state = network.initServer(port);
+  network.runServer(state, config);
   if (state == false)
     {
       cerr << "Error : rv < 0" << endl;
